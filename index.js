0bff1490005011527ab4c18895505c5109a64fd5
Andrew Pashinnik <tobirawork@gmail.com>
2019-07-27 00:01:01 +0300

    fix

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-let WELCOME_MESSAGE = '';
-
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true, timeout: 500 });
+let WELCOME_MESSAGE = 'Привет {kroshka}! Прочитай гайды в закрепе. Это чат потока. Тут около 15+ 2-3 курсников и люди с твоего потока.  Мы тут только что ты помогать и травить байки)';
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+    polling: true,
+    timeout: 500
+});
 
 bot.on('message', msg =>{
     if (msg.new_chat_members !== undefined) {
         console.log(msg.new_chat_member.username);
         console.log(msg.new_chat_member.id);
 
-        const text = WELCOME_MESSAGE.replace('{$kroshka}', `@${msg.new_chat_member.username}`);
+        const text = WELCOME_MESSAGE.replace('{kroshka}', `@${msg.new_chat_member.username}`);
 
         bot.sendMessage(msg.chat.id, text);
     }
 });
 
-bot.onText(/\/change_text (.+) (.+)/, function onEchoText(msg, match) {
-    const [, password, newText] = match[1];
+bot.onText(/\/change_text (.+) (.+)/, function (msg, match) {
+    const [, password, newText] = match;
 
     if (password === (process.env.PASS || 'legezza_')) {
         WELCOME_MESSAGE = newText;
