1f342ca9bb12cb5061cb54f9d979cb1b986cc23b
Андрей <anderkoval3@gmail.com>
2019-08-02 12:42:53 +0300

    cat timeout

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const fs = require('fs');
-
 let greeting = fs.readFileSync('./greeting.txt', { encoding: 'utf-8' });
+
+var catTimeOutSeconds = Number(process.env.CAT_TIMEOUT_SECONDS) || 50
+var isCatAllowed = true
+
 
 bot.on('new_chat_members', (ctx) => {
     const userName = `${ctx.update.message.new_chat_member.first_name || ''} ${ctx.update.message.new_chat_member.last_name || ''}`;
@@ -14,7 +17,7 @@ bot.on('new_chat_members', (ctx) => {
     ctx.replyWithMarkdown(text);
 })
 
-bot.hears(/^\/change_text (\S+) ([\s\S]*)$/gm, (ctx) => {
+bot.hears(/^\/set_text (\S+) ([\s\S]*)$/gm, (ctx) => {
     const [, password, newText] = ctx.match;
     if (password === (process.env.PASS || 'apirol')) {
         try {
@@ -27,20 +30,42 @@ bot.hears(/^\/change_text (\S+) ([\s\S]*)$/gm, (ctx) => {
     }
 })
 
+bot.hears(/^\/set_timeout (\S+) (\d+)$/gm, (ctx) => {
+    const [, password, newSeconds] = ctx.match;
+    console.log(ctx.match)
+    if (password === (process.env.PASS || 'apirol')) {
+        try {
+            ctx.replyWithMarkdown(`Время изменено на ${newSeconds} секунд`);
+            catTimeOutSeconds = Number(newSeconds);
+            isCatAllowed = true;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+})
+
+
 bot.command('cat', (ctx) => {
+    if (!isCatAllowed) {
+        // можно как то переделать, команду какую то сделать и тд
+        ctx.reply(`Подождите ${catTimeOutSeconds} секунд с момента прошлой команды`, Extra.inReplyTo(ctx.message.message_id))
+        return
+    }
     var request = require('request');
     request('https://api.thecatapi.com/v1/images/search', (error, response, body) => {
     if (response.statusCode != 200) {
         console.error(error);
     } else {
-        let imageUrl = JSON.parse(body)[0]['url']
-        ctx.replyWithPhoto({url: imageUrl}, Extra.inReplyTo(ctx.message.message_id))
+        let imageUrl = JSON.parse(body)[0]['url'];
+        ctx.replyWithPhoto({url: imageUrl}, Extra.inReplyTo(ctx.message.message_id));
+        isCatAllowed = false;
+        setTimeout(() => isCatAllowed = true, catTimeOutSeconds * 1000)
     }
 });
 })
 
 bot.catch((err) => {
-    console.error(err)
+    console.error(err)  
 })
 
 bot.launch()    
